a6b22094ad97183cbf3d3934aca045c5729ba022
bobolast3 <bobolast3@gmail.com>
2023-07-24 16:17:16 -0700

    Your commit message

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <starg.h>
+#include <stdarg.h>
 
 /**
  * _printf - Custom implementation of printf function
@@ -14,53 +14,45 @@
  * Return: The number of characters printed
  * (excluding the null byte used to end output to strings).
  */
+#include <stdio.h>
+#include <stdarg.h>
+
+
 int _printf(const char *format, ...)
 {
-va_list args;
-va_start(args, format);
-int count = 0;
-char c;
-const char *str;
-
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-c = va_arg(args, int);
-putchar(c);
-count++;
-break;
-case 's':
-str = va_arg(args, const char*);
-while (*str != '\0')
-{
-putchar(*str);
-str++;
-count++;
-}
-break;
-case '%':
-putchar('%');
-count++;
-break;
-default:
-putchar('%');
-putchar(*format);
-count += 2;
-break;
-}
-}
-else
-{
-putchar(*format);
-count++;
-}
-format++;
-}
-va_end(args);
-return (count);
+    int count = 0;
+    char c, *str;
+    va_list args;
+    va_start(args, format);
+    
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    c = va_arg(args, int);
+                    putchar(c), count++;
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    while (*str)
+                        putchar(*str), str++, count++;
+                    break;
+                case '%':
+                    putchar('%'), count++;
+                    break;
+                default:
+                    putchar('%'), putchar(*format), count += 2;
+                    break;
+            }
+        }
+        else
+            putchar(*format), count++;
+        format++;
+    }
+    va_end(args);
+    return count;
 }
