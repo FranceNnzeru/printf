16da0285dd09cb103345a37e28ebdfe947d40283
bobolast3 <bobolast3@gmail.com>
2023-07-24 16:35:03 -0700

    mabuela

--- _printf.c
+++ _printf.c
@@ -16,37 +16,38 @@
 int _printf(const char *format, ...)
 {
 int count = 0;
-char c, *str;
-va_list args;
-va_start(args, format);
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-c = va_arg(args, int);
-putchar(c), count++;
-break;
-case 's':
-str = va_arg(args, char *);
-while (*str)
-    putchar(*str), str++, count++;
-break;
-case '%':
-putchar('%'), count++;
-break;
-default:
-putchar('%'), putchar(*format), count += 2;
-break;
-}
-}
-else
-putchar(*format), count++;
-format++;
-}
-va_end(args);
-return (count);
+    char c, *str;
+    va_list args;
+    va_start(args, format);
+    
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    c = va_arg(args, int);
+                    putchar(c), count++;
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    while (*str)
+                        putchar(*str), str++, count++;
+                    break;
+                case '%':
+                    putchar('%'), count++;
+                    break;
+                default:
+                    putchar('%'), putchar(*format), count += 2;
+                    break;
+            }
+        }
+        else
+            putchar(*format), count++;
+        format++;
+    }
+    va_end(args);
+    return (count);
 }
