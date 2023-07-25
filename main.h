04dff5eae8c8e1d55335c170749a5041dfd5918e
FranceNnzeru <Fnnzeru@gmail.com>
2023-07-25 05:52:05 -0700

    mabuela

--- main.h
+++ main.h
@@ -4,4 +4,6 @@ int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_integer(va_list args);
+int print_binary(va_list args);
+int print_conversion_specifier(char specifier, va_list args)
 #endif /* MAIN_H */
