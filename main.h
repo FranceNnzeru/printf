3e4b978d99ee83c0195f2c046392260896dc108a
FranceNnzeru <Fnnzeru@gmail.com>
2023-07-25 07:37:27 -0700

    mabuela

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ int print_string(va_list args);
 int print_integer(va_list args);
 int print_binary(va_list args);
 int print_conversion_specifier(char specifier, va_list args);
-int print_unsigned(va_list args);
 int print_octal(va_list args);
 int print_hex(va_list args, int uppercase);
+int print_unsigned(va_list args);
 #endif /* MAIN_H */
